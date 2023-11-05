dcea4eb0be2744c59d14c6199d70a675e06a7f0a
radelytskyipavlo <145326273+radelytskyipavlo@users.noreply.github.com>
2023-11-06 00:02:18 +0200

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.3/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 2.0;
+			double expected = sin(x) / (cos(x) + 2.0);
+			double actual = g(x);
+		}
+	};
+}
